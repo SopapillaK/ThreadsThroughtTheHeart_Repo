a1d2a786eae2638ddb04e777652336c032267b3b
SopapillaKat <vellita2011@gmail.com>
2025-01-22 16:52:37 -0700

    understanding in, shouting state in

--- ThreadsThroughTheHeart_Project/Assets/Scripts/FrayShoutState.cs
+++ ThreadsThroughTheHeart_Project/Assets/Scripts/FrayShoutState.cs
@@ -1,36 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Threading;
 using UnityEngine;
 
-public class FrayAttackState : StateMachineBehaviour
+public class FrayShoutState : StateMachineBehaviour
 {
-    Transform player;
     float timer;
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
         timer = 0;
+
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.transform.LookAt(player);
-        float distance = Vector3.Distance(player.position, animator.transform.position);
-        if (distance > 3.5f)
-        {
-            animator.SetBool("isAttacking", false);
-            Debug.Log("attack to chasing anim");
-            timer += Time.deltaTime;
-
-            
-        }
         timer += Time.deltaTime;
-        if (timer > 3)
-            animator.SetBool("isShouting", true);
+        if (timer > 5)
+            animator.SetBool("isShouting", false);
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
