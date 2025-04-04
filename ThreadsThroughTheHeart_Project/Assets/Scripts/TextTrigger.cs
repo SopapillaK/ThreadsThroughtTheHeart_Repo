f6f851464f5cd2d7b38d7d9b8cf21ef0ef7dd279
SopapillaKat <heyitssoph1234@gmail.com>
2025-04-04 10:55:30 -0700

    realigned fray in new house, added text to direct player in hearthub, made camera the same in hearthub, updated player prefab to current version

--- ThreadsThroughTheHeart_Project/Assets/Scripts/TextTrigger.cs
+++ ThreadsThroughTheHeart_Project/Assets/Scripts/TextTrigger.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class TextTrigger : MonoBehaviour
 {
     public GameObject UnderstandingChoices;
-    
+    public GameObject directionText;
+
     void OnTriggerEnter()
     {
         UnderstandingChoices.SetActive(true);
+        directionText.SetActive(false);
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
         //Time.timeScale = 0;
